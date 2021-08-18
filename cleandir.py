c1281acb7cfcba147a8fcf1cea0a5bbcf7b35014
ronmarian7 <ronmarian7@gmail.com>
2021-08-18 13:41:03 +0300

    update

--- cleandir.py
+++ cleandir.py
@@ -1,28 +1,26 @@
 import os
 import shutil
+import extantions
 
 
-EXT_AUDIO = ['.wav', '.mp3', '.raw', '.aac', '.wav', '.aif', '.cda', '.mpa', '.wpl']
-EXT_VIDEO = ['.mp4', '.mia', '.m4v', '.f4vv', '.f4a', '.m4b', '.m4r', '.avi', '.wma', '.flv']
-EXT_IMAGES = ['.jpeg', '.png', '.svg', '.gif', '.bmp', '.nef', '.jpg']
-EXT_DOCUMENTS = ['.txt', '.pdf', '.doc', '.docx', '.odt', '.html']
-EXT_PPT = ['.ppt', '.pptx', '.pps']
-EXT_EXCEL = ['.xlsx', '.xls']
-EXT_ZIP = ['.zip', '.rar']
-EXT_INSTALL = ['.exe', '.iso', '.bin']
+#def insert_file_to_destination_dir(file, downloads_dir):
+#    dest_path = os.path.join(downloads_dir, file)
+#    if os.path.isdir(dest_path):
+
 
 print('DOWNLOADS FOLDER CLEANUP')
-os.chdir('C:\\Users\\ronma\\Downloads')
-print('Current directory: {}' .format(os.getcwd()))
+downloads_dir = 'C:\\Users\\ronma\\Downloads'
+os.chdir(downloads_dir)
+print(f'Current directory: {os.getcwd()}')
 
-# Show you all the files in the directory
+# List all the files in the downloads directory
 files = os.listdir()
 
 # Create directories if they don't exist
 DIRS = ['Audio', 'Video', 'Images', 'Documents', 'Installation', 'Folders', 'PowerPoint', 'Excel', 'Other', 'Zip']
 if not os.path.isdir('./Audio'):
     for d in DIRS:
-        os.mkdir('./{}'.format(d))
+        os.mkdir(f'./{d}')
 
     print('Directories created successfully')
 
@@ -31,28 +29,29 @@ for f in files:
     if f != 'desktop.ini':
         name, extension = os.path.splitext(f)
 
-        if extension in EXT_IMAGES:
-            shutil.move(f, './images/{}'.format(f))
-        elif extension in EXT_AUDIO:
-            shutil.move(f, './audio/{}'.format(f))
-        elif extension in EXT_PPT:
-            shutil.move(f, './powerpoint/{}'.format(f))
-        elif extension in EXT_EXCEL:
-            shutil.move(f, './excel/{}'.format(f))
-        elif extension in EXT_ZIP:
-            shutil.move(f, './zip/{}'.format(f))
-        elif extension in EXT_VIDEO:
-            shutil.move(f, './video/{}'.format(f))
-        elif extension in EXT_DOCUMENTS:
-            shutil.move(f, './documents/{}'.format(f))
-        elif extension in EXT_INSTALL:
-            shutil.move(f, './Installation/{}'.format(f))
+
+        if extension in extantions.EXT_IMAGES:
+            shutil.move(f, f'./images/{f}')
+        elif extension in extantions.EXT_AUDIO:
+            shutil.move(f, f'./audio/{f}')
+        elif extension in extantions.EXT_PPT:
+            shutil.move(f, f'./powerpoint/{f}')
+        elif extension in extantions.EXT_EXCEL:
+            shutil.move(f, f'./excel/{f}')
+        elif extension in extantions.EXT_ZIP:
+            shutil.move(f, f'./zip/{f}')
+        elif extension in extantions.EXT_VIDEO:
+            shutil.move(f, f'./video/{f}')
+        elif extension in extantions.EXT_DOCUMENTS:
+            shutil.move(f, f'./documents/{f}')
+        elif extension in extantions.EXT_INSTALL:
+            shutil.move(f, f'./Installation/{f}')
         else:
             if os.path.isdir(name):
                 if name not in DIRS:
-                    shutil.move(f, './folders/{}'.format(f))
+                    shutil.move(f, f'./folders/{f}')
             else:
-                shutil.move(f, './other/{}'.format(f))
+                shutil.move(f, f'./other/{f}')
 
 print('CLEANUP COMPLETED')
 
---
+++ extantions.py
@@ -0,0 +1,8 @@
+EXT_AUDIO = ['.wav', '.mp3', '.raw', '.aac', '.wav', '.aif', '.cda', '.mpa', '.wpl']
+EXT_VIDEO = ['.mp4', '.mia', '.m4v', '.f4vv', '.f4a', '.m4b', '.m4r', '.avi', '.wma', '.flv', '.mkv']
+EXT_IMAGES = ['.jpeg', '.png', '.svg', '.gif', '.bmp', '.nef', '.jpg']
+EXT_DOCUMENTS = ['.txt', '.pdf', '.doc', '.docx', '.odt', '.html']
+EXT_PPT = ['.ppt', '.pptx', '.pps']
+EXT_EXCEL = ['.xlsx', '.xls']
+EXT_ZIP = ['.zip', '.rar']
+EXT_INSTALL = ['.exe', '.iso', '.bin', '.torrent']
